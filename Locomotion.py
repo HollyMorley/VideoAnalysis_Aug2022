2ea3c6159ef9028c08e7e3b1b9c05e5f47858850
Holly MORLEY <58185156+HollyMorley@users.noreply.github.com>
2023-02-27 15:35:48 +0000

    Update with copy of previous attempt using x velocity. Will delete this after this commit

--- Locomotion.py
+++ Locomotion.py
@@ -9,6 +9,7 @@ import Plot
 import scipy
 from scipy.signal import savgol_filter
 from scipy import stats
+from tqdm import tqdm
 
 
 class Locomotion():
@@ -16,7 +17,7 @@ class Locomotion():
     def __init__(self):
         super().__init__()
 
-    def getLocoPeriods(self, data, con, mouseID, view):
+    def getLocoPeriods(self, data, con, mouseID, view='Side'):
         # limbMask = {
         #     'Toe': {
         #         'ForepawToeR': data[con][mouseID][view].loc(axis=1)['ForepawToeR', 'likelihood'] > pcutoff,
@@ -41,23 +42,29 @@ class Locomotion():
 
         beltspeeds = utils.Utils().getSpeedConditions(con=con)
 
-        limblist = ['ForepawToeR', 'ForepawToeL', 'HindpawToeR', 'HindpawToeL', 'ForepawAnkleR', 'ForepawAnkleL', 'HindpawAnkleR', 'HindpawAnkleL']
+        limblist = ['ForepawToeR', 'ForepawToeL', 'HindpawToeR', 'HindpawToeL']
 
-        for r in data[con][mouseID][view].index.get_level_values(level='Run').unique():
-            belt1speed = None
-            belt2speed = None
-            for l in limblist:
+        for l in tqdm(limblist):
+        #for l in limblist:
+            StepCycleAll = []
+            for r in data[con][mouseID][view].index.get_level_values(level='Run').unique():
+                belt1speed = None
+                belt2speed = None
+
                 mask = data[con][mouseID][view].loc(axis=0)[r].loc(axis=1)[l, 'likelihood'] > pcutoff
                 x = data[con][mouseID][view].loc(axis=0)[r].loc(axis=1)[l, 'x'].values[mask]
                 t = data[con][mouseID][view].loc(axis=0)[r].loc(axis=1)[l, 'x'].index.get_level_values(level='FrameIdx')[mask]
                 # Smooth the x-coordinate data
                 x_smooth = savgol_filter(x, window_length=20, polyorder=10)  # x_smooth = savgol_filter(x, window_length=5, polyorder=3)
 
+                ############ TEMP##########
+                x_smooth = x
+
                 # Calculate the velocity
                 velocity = np.gradient(x_smooth, t)
 
                 ##### find first stance frames and first swing frames
-                markerstuff = GetRuns.GetRuns().findMarkers(data[con][mouseID][view].loc(axis=0)[r])
+                markerstuff = GetRuns.GetRuns().findMarkers(data[con][mouseID][view])
 
                 if 'APAChar' in con or 'Perception' in con: ############################ !!!!!!!!!!!!!! COULD ADD IN HERE AN AND STATEMENT TO SAY AND NO E.G. REPEAT OR EXTENDED TO SIGNIFY ITS THE ORIGINAL EXP FORMAT !!!!!!!!!!!!! #########################
                     # define perception test as always low-high but this isnt in con name
@@ -91,8 +98,8 @@ class Locomotion():
 
                 # now need to specify the velocity threshold based on if each limb is past the transition line
                 beltmask = x_smooth > markerstuff['DualBeltMarkers']['x_edges'][3] # not sure if i should use x_smooth or raw x here
-                belt1speed_px = (markerstuff['cmtopx'] * belt1speed) / fps
-                belt2speed_px = (markerstuff['cmtopx'] * belt2speed) / fps
+                belt1speed_px = ((markerstuff['cmtopx'] * belt1speed) / fps) + 1
+                belt2speed_px = ((markerstuff['cmtopx'] * belt2speed) / fps) + 1
                 prethreshmask = velocity[~beltmask] > belt1speed_px
                 postthreshmask = velocity[beltmask] > belt2speed_px
                 swingthreshmask = np.concatenate((prethreshmask,postthreshmask))
@@ -100,16 +107,15 @@ class Locomotion():
                 # find Swing
                 window_size = 6
                 rolling_swing = np.zeros(np.shape(swingthreshmask))
-                rolling_swing[:-window_size + 1] = np.sum(np.vstack([swingthreshmask[:window_size][::-1], np.vstack([swingthreshmask[i:i - window_size:-1] for i in range(window_size, len(swingthreshmask))])]), axis=1)
+                rolling_swing[:-window_size + 1] = np.sum(np.vstack([swingthreshmask[:window_size][::-1], np.vstack([swingthreshmask[i:i - window_size:-1] for i in range(window_size, len(swingthreshmask))])]), axis=1) # chunk each value into it and it's next 5 values and then sum how many of the values are above the swing threshold
                 # Create a mask for the positions where the pattern occurs
-                swingmask = np.logical_and(rolling_swing == 5, np.logical_not(swingthreshmask))
+                swingmask = np.logical_and(rolling_swing == 5, np.logical_not(swingthreshmask)) # finds values where there is a negative value succeeded by 5 positive values
                 # Find the indices where the mask is True and then pick the next one as this is the first True value where the previous value is False and the next 4 are True
                 swingpositions = np.where(swingmask)[0] + 1
 
                 # find Stance
                 rolling_stance = np.zeros(np.shape(swingthreshmask))
-                rolling_stance[window_size - 1:] = np.sum(np.vstack([swingthreshmask[:window_size][::-1], np.vstack(
-                    [swingthreshmask[i:i - window_size:-1] for i in range(window_size, len(swingthreshmask))])]), axis=1)
+                rolling_stance[window_size - 1:] = np.sum(np.vstack([swingthreshmask[:window_size][::-1], np.vstack([swingthreshmask[i:i - window_size:-1] for i in range(window_size, len(swingthreshmask))])]), axis=1)
                 # Create a mask for the positions where the pattern occurs
                 stancemask = np.logical_and(rolling_stance == 5, np.logical_not(swingthreshmask))
                 # Find the indices where the mask is True and then pick the next one as this is the first True value where the previous value is False and the next 4 are True
@@ -118,6 +124,7 @@ class Locomotion():
                 # put first swing and stance frames into df
                 Stance = np.full([len(data[con][mouseID][view].loc(axis=0)[r])], np.nan)
                 Swing = np.full([len(data[con][mouseID][view].loc(axis=0)[r])], np.nan)
+                Cycle = np.full([len(data[con][mouseID][view].loc(axis=0)[r])], np.nan)
                 Stanceidx = t[stancepositions]
                 Swingidx = t[swingpositions]
                 swingmask = np.isin(data[con][mouseID][view].loc(axis=0)[r].index.get_level_values(level='FrameIdx').values, Swingidx.values)
@@ -125,9 +132,58 @@ class Locomotion():
                 stancemask = np.isin(data[con][mouseID][view].loc(axis=0)[r].index.get_level_values(level='FrameIdx').values, Stanceidx.values)
                 Stance[stancemask] = 1
 
-                for view in ['Side', 'Front', 'Overhead']:
-                    data[con][mouseID][view].loc[r,(l,'Swing')] = Swing
-                    data[con][mouseID][view].loc[r, (l, 'Stance')] = Stance
+                Cycle[stancemask] = 0
+                Cycle[swingmask] = 1
+
+                if not np.any(swingmask & stancemask):
+                    lastval = np.nan
+                    newcycle = np.full([len(Cycle)], np.nan)
+                    for i, val in enumerate(Cycle):
+                        if val == 0 or val == 1:
+                            lastval = val
+                            newcycle[i] = val
+                        elif np.isnan(val):
+                            newcycle[i] = lastval
+                else:
+                    raise ValueError('There is overlap between detection of first stance and first swing frame! This can''t be possible')
+
+                StepCycleAll.append(newcycle)
+
+
+                ############################################################################################################
+                # new swing/ stance id
+                belt1speed = None
+                belt2speed = None
+                mask = data[con][mouseID][view].loc(axis=0)[r, ['RunStart', 'Transition', 'RunEnd']].loc(axis=1)[
+                           l, 'likelihood'] > pcutoff
+                x = data[con][mouseID][view].loc(axis=0)[r, ['RunStart', 'Transition', 'RunEnd']].loc(axis=1)[
+                    l, 'x'].values[mask]
+                t = data[con][mouseID][view].loc(axis=0)[r, ['RunStart', 'Transition', 'RunEnd']].loc(axis=1)[
+                    l, 'x'].index.get_level_values(level='FrameIdx')[mask]
+                # Smooth the x-coordinate data
+                x_smooth = savgol_filter(x, window_length=20,
+                                         polyorder=10)  # x_smooth = savgol_filter(x, window_length=5, polyorder=3)
+                ############ TEMP##########
+                x_smooth = x
+                # Calculate the velocity
+                velocity = np.gradient(x_smooth, t)
+
+                slope, intercept, r_value, p_value, std_err = stats.linregress(t, x)
+                xcorr = (x - slope * t) - intercept
+                # find start of stance
+                peaks_st, _ = scipy.signal.find_peaks(xcorr, rel_height=1, width=5, distance=10)
+                stancestart = t[peaks_st] # find first frame
+                # find start of swing
+                peaks_sw, _ = scipy.signal.find_peaks(-xcorr,  rel_height=1, width=15, distance=10)
+                swingstart = t[peaks_sw]
+                ############################################################################################################
+
+            StepCycleAll_flt = np.concatenate(StepCycleAll).ravel()
+            data[con][mouseID][view].loc(axis=1)[l, 'StepCycle'] = StepCycleAll_flt
+                # for view in ['Side', 'Front', 'Overhead']:
+                #     # data[con][mouseID][view].loc[r,(l,'Swing')] = Swing
+                #     # data[con][mouseID][view].loc[r, (l, 'Stance')] = Stance
+                #     data[con][mouseID][view].loc[r, (l, 'StepCycle')] = newcycle
 
 
 

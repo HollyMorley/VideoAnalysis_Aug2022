5e0e1f66082656a33feff7870f85c07f48b81764
Holly MORLEY <58185156+HollyMorley@users.noreply.github.com>
2023-11-03 15:16:56 +0000

    Update to Structural_calculations.py

--- Helpers/Structural_calculations.py
+++ Helpers/Structural_calculations.py
@@ -1,4 +1,7 @@
 import numpy as np
+import pandas as pd
+import cv2
+import warnings
 from scipy import stats
 
 class GetRealDistances:
@@ -7,7 +10,7 @@ class GetRealDistances:
         self.con = con
         self.mouseID = mouseID
 
-    def get_belt_coordinates(self, position):
+    def get_belt_coordinates(self, position, view):
         """
         Retrieve the pixel coordinates in x- and y-axis for left and right positions on either the start or transition position on the belt.
         :param data: dict of all dfs for each experiment
@@ -20,44 +23,72 @@ class GetRealDistances:
         label_name = 'StartPlat' if position == 'start' else 'Transition'
 
         # y dimension (aka depth of the travellator)
-        yax_L = []
-        yax_R = []
+        y_L = []
+        y_R = []
         # x dimension (aka length of the travellator)
-        xax_L = []
-        xax_R = []
+        x_L = []
+        x_R = []
 
         # calculate mean of each coordinate across all runs
-        for r in self.data[self.con][self.mouseID]['Front'].index.get_level_values(level='Run').unique().astype(int):
+        for r in self.data[self.con][self.mouseID][view].index.get_level_values(level='Run').unique().astype(int):
             # retrieve y axis data
-            yax_L.append(
-                np.mean(self.data[self.con][self.mouseID]['Front'].loc(axis=0)[r, 'TrialStart'].loc(axis=1)['%sL'%label_name, 'x']))
-            yax_R.append(
-                np.mean(self.data[self.con][self.mouseID]['Front'].loc(axis=0)[r, 'TrialStart'].loc(axis=1)['%sR'%label_name, 'x']))
+            y_L.append(
+                np.mean(self.data[self.con][self.mouseID][view].loc(axis=0)[r, 'TrialStart'].loc(axis=1)['%sL'%label_name, 'y']))
+            y_R.append(
+                np.mean(self.data[self.con][self.mouseID][view].loc(axis=0)[r, 'TrialStart'].loc(axis=1)['%sR'%label_name, 'y']))
 
             # retrieve x axis data
-            xax_L.append(
-                np.mean(self.data[self.con][self.mouseID]['Side'].loc(axis=0)[r, 'TrialStart'].loc(axis=1)['%sL'%label_name, 'x']))
-            xax_R.append(
-                np.mean(self.data[self.con][self.mouseID]['Side'].loc(axis=0)[r, 'TrialStart'].loc(axis=1)['%sR'%label_name, 'x']))
+            x_L.append(
+                np.mean(self.data[self.con][self.mouseID][view].loc(axis=0)[r, 'TrialStart'].loc(axis=1)['%sL'%label_name, 'x']))
+            x_R.append(
+                np.mean(self.data[self.con][self.mouseID][view].loc(axis=0)[r, 'TrialStart'].loc(axis=1)['%sR'%label_name, 'x']))
 
-        L_yax_mean = np.mean(yax_L)
-        R_yax_mean = np.mean(yax_R)
-        L_xax_mean = np.mean(xax_L)
-        R_xax_mean = np.mean(xax_R)
+        L_y_mean = np.mean(y_L)
+        R_y_mean = np.mean(y_R)
+        L_x_mean = np.mean(x_L)
+        R_x_mean = np.mean(x_R)
 
 
         coords = {
             'L': {
-                'x': L_xax_mean,
-                'y': L_yax_mean
+                'x': L_x_mean,
+                'y': L_y_mean
             },
             'R': {
-                'x': R_xax_mean,
-                'y': R_yax_mean
+                'x': R_x_mean,
+                'y': R_y_mean
             }
         }
 
         return coords
+
+    def assign_coordinates(self):
+        """
+
+        :return:
+        """
+        start_coords_front = self.get_belt_coordinates(position='start', view='Front')
+        trans_coords_front = self.get_belt_coordinates(position='trans', view='Front')
+        start_coords_side = self.get_belt_coordinates(position='start', view='Side')
+        trans_coords_side = self.get_belt_coordinates(position='trans', view='Side')
+
+        s = {
+            '0': {'x': start_coords_side['R']['x'], 'y': start_coords_side['R']['y']},
+            '1': {'x': start_coords_side['L']['x'], 'y': start_coords_side['L']['y']},
+            '2': {'x': trans_coords_side['L']['x'], 'y': trans_coords_side['L']['y']},
+            '3': {'x': trans_coords_side['R']['x'], 'y': trans_coords_side['R']['y']}
+        }
+
+        f = {
+            '0': {'x': start_coords_front['R']['x'], 'y':  start_coords_front['R']['y']},
+            '1': {'x': start_coords_front['L']['x'], 'y': start_coords_front['L']['y']},
+            '2': {'x': trans_coords_front['L']['x'], 'y': trans_coords_front['L']['y']},
+            '3': {'x': trans_coords_front['R']['x'], 'y':  trans_coords_front['R']['y']}
+        }
+
+        return s, f
+
+
 
     def calculate_pixel_size(self, left, right, real_size):
         """
@@ -73,23 +104,47 @@ class GetRealDistances:
 
         return px_mm
 
-    def interpolate_pixel_size(self, axis, real_size):
+    def get_true_xy(self, src_coords, dst_coords, point_coords):
+        h, status = cv2.findHomography(src_coords, dst_coords)
+        point_coords = point_coords.reshape(3, 1)
+        transformed_point_coords = np.dot(h, point_coords)
+        transformed_point_coords = transformed_point_coords / transformed_point_coords[2]
+        x = transformed_point_coords[0]
+        y = transformed_point_coords[1]
+
+        return x, y
+
+
+    def interpolate_pixel_size_front_cam(self, real_size):
         """
 
         :param axis:  Axis of view point, where the other axis is the one which the area a pixel represents changes, e.g. in 'Side' or axis='x', the y-axis of the 3D representation represents the depth
         :param real_size:
         :return:
         """
-        start_coords = self.get_belt_coordinates(position='start')
-        trans_coords = self.get_belt_coordinates(position='trans')
+        warnings.filterwarnings("ignore", category=pd.errors.PerformanceWarning)
 
-        if axis == 'y':
+        s, f = self.assign_coordinates()
 
-            near_px_size = self.calculate_pixel_size(left=trans_coords['R']['y'], right=trans_coords['L']['y'], real_size=real_size)
-            far_px_size = self.calculate_pixel_size(left=start_coords['R']['y'], right=start_coords['L']['y'], real_size=real_size)
-        elif axis == 'x':
-            near_px_size = self.calculate_pixel_size(left=start_coords['R']['x'], right=trans_coords['R']['x'], real_size=real_size)
-            far_px_size = self.calculate_pixel_size(left=start_coords['L']['x'], right=trans_coords['L']['x'], real_size=real_size)
+        near_px_size = self.calculate_pixel_size(left=f['3']['x'], right=f['2']['x'], real_size=real_size)
+        far_px_size = self.calculate_pixel_size(left=f['0']['x'], right=f['1']['x'], real_size=real_size)
 
-        slopeL, interceptL, r_valueL, p_valueL, std_errL = stats.linregress(
-            [start_coords['L']['x'], trans_coords['L']['x']], [start_px_size, trans_px_size])
+        displ_rect_coords = np.array([[s['0']['x'], s['0']['y']], [s['3']['x'], s['3']['y']], [s['2']['x'], s['2']['y']], [s['1']['x'], s['1']['y']]])
+        displ_rect_coords[:, 1] = -displ_rect_coords[:, 1]
+        corr_rect_coords = np.array([[s['0']['x'], s['0']['y']], [s['3']['x'],  s['0']['y']], [s['3']['x'], s['1']['y']], [s['0']['x'], s['1']['y']]])
+        corr_rect_coords[:, 1] = -corr_rect_coords[:, 1]
+
+        true_x, true_y = self.get_true_xy(displ_rect_coords, corr_rect_coords, )
+
+        # slopeL, interceptL, r_valueL, p_valueL, std_errL = stats.linregress(
+        #     [start_coords['L']['x'], trans_coords['L']['x']], [start_px_size, trans_px_size])
+
+
+        # take y value from front
+        # find corresponding x in corrected rect
+        # find real x by transforming to true side rectangle
+
+
+
+
+        #### do same for side view?????????

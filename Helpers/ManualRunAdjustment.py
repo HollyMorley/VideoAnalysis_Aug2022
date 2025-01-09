448443f09b56f040ea7633fdb254fd81e759dec4
HMorley <58185156+HollyMorley@users.noreply.github.com>
2025-01-09 20:40:51 +0000

    adding in manual adjustments to runs based on paper archives

--- Helpers/ManualRunAdjustment.py
+++ Helpers/ManualRunAdjustment.py
@@ -11,6 +11,9 @@ runs_to_drop_placeholder = { # first = 0, last = 40 + 2/5
     '20230310': {
         '1035245': [28,29,30,31], # mouse got distressed and all these runs were a mess (extras have also been removed)
     },
+    '20230317': {
+        '1035244': [2,3], # belt was off for the first 2 runs
+    },
 }
 
 runs_to_drop_completely = {
@@ -25,7 +28,7 @@ runs_to_drop_completely = {
     },
     '20230309': {
         '1035299': [42], # mouse was being difficult, seems like an accidental extra run
-        '1035301': [6], # belt was off at 4:42 so jerry re-attempted
+        '1035301': [6], # belt was off at 4:42 (this was the 'extra')
     },
     '20230310': {
         '1035243': [12], # jerry forgot to change the belt speed at 6:02 (this was the 'extra')
@@ -35,6 +38,23 @@ runs_to_drop_completely = {
     '20230312': {
         '1035297': [32,41], # 1) extra run as messed up the first run, 2) failed run attempt but had runbacks so was captured on belt (!! possible this wasnt registered though)
         '1035298': [34], # mouse climbed out and back on to belt mid-way so not normal run, so jerry re-attempted
+    },
+    '20230314': {
+        '1035297': [16,40], # 1) belt was off at 7:09 (this was the 'extra'), 2) belt was off at 20:55 (this was the 'extra')
+        '1035298': [2], # belt was off for the 1st baseline run (this was the 'extra')
+    },
+    '20230316': {
+        '1035245': [33,34,35], # mouse failed 3 times in a row so jerry re-attempted
+    },
+    '20230317': {
+        '1035243': [14,15,16,17], # 4 fails where mouse was on belt so jerry re-attempted
+        '1035244': [4,5,6,7,8,17,18,19,20], # mouse failed at [4,5,6,7,8] so jerry re-attempted, then 1 failed at 27:01, then 3 habituation runs mid-way through
+    },
+    '20230318': {
+        '1035249': [2], # first real run was a fail so jerry re-attempted
+    },
+    '20230319': {
+        '1035244': [42], # jerry knocked the door out at r29 so he did an extra but i think its fine so have just removed the final run
     },
 
 }
@@ -49,7 +69,10 @@ missing_runs = {
     },
     '20230309': {
             '1035298': [0,1], # missing the 2 habituation runs
-        }
+    },
+    '20230319': {
+        '1035249': [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27], # 28 runs missing from start
+    },
 }
 
 '''
@@ -59,6 +82,8 @@ missing_runs = {
 20230309 - 1035299: slid under door at 3:31/after r6, also slight incorrect door close at 9:40/after r16 and 14:46/after
  r23, door close and mouse slip under at 20:43/after r31
 20230309 - 1035302: slid under door at 3:07/after r5
+20230316 - 1035250: did a extra run at 17:39 with a door open/close but dont think this would have been registered
+
 
 '''
 
